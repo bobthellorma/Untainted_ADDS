235057bcf71d5704b093cc7dc28cd0baed0a46c2
bobthellorma <98739156+bobthellorma@users.noreply.github.com>
2022-08-31 12:19:49 +0930

    start of prac 5

---
+++ .vscode/equal.cpp
@@ -0,0 +1,4 @@
+// write a friend function called equal that checks whether two books are equal
+// true if equal, false otherwise
+//Two books are equal if they have the same author surname, author initials, book title and publication date. 
+
