05db0bb12740b7827723d7b6189f012d0b13b417
bobthellorma <98739156+bobthellorma@users.noreply.github.com>
2022-09-14 13:17:24 +0930

    final submission

--- numDup_head.cpp
+++ numDup_head.cpp
@@ -1,5 +1,3 @@
-#include <iostream> // for debugging
-
 int numDup(int arr[], int start, int size, int element) {
     int dup = 0;
 
@@ -18,10 +16,4 @@ int numDup(int arr[], int start, int size, int element) {
     // Call Recursive
     return dup + numDup(arr,start+1,size,element);
 
-}
-
-int main()
-{
-    int arr[] = {1,4,0,7,8,0,1,9,0};
-    std::cout << numDup(arr,3,9,0) << std::endl;
 }
--- numDup_tail.cpp
+++ numDup_tail.cpp
@@ -1,4 +1,6 @@
-#include <iostream> // for debugging
+/*
+Implemented using a helper function
+*/
 
 int numDupTail(int arr[], int start, int size, int element, int counter)
 {
@@ -16,16 +18,9 @@ int numDupTail(int arr[], int start, int size, int element, int counter)
 
     // Call Recursive
     return numDupTail(arr,start+1,size,element,counter);
-
 }
 
 int numDup(int arr[], int start, int size, int element)
 {
     return numDupTail(arr,start,size,element,0);
-}
-
-int main()
-{
-    int arr[] = {1,4,0,7,8,0,1,9,0};
-    std::cout << numDup(arr,1,9,0) << std::endl;
 }
