724d11c9e04a288cd10653e52a8beaf339394452
bobthellorma <98739156+bobthellorma@users.noreply.github.com>
2022-10-09 11:23:57 +1030

    changed method of input

--- main.cpp
+++ main.cpp
@@ -12,16 +12,15 @@ int main()
     RecursiveBinarySearch c;
 
     // take in list
-    /*
     std::vector<int> listval;
     int tempa;
     while (std::cin >> tempa)
     {
         listval.push_back(tempa);
     }
-    */
+    
 
-    std::vector<int> listval = {0,3,5,4,-5,100,7777,2014};
+    //std::vector<int> listval = {0,3,5,4,-5,100,7777,2014};
 
     // QuickSort list
     std::vector<int> sorted = b.sort(listval);
