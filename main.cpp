e1bebd23e92b255090527cc3ecd87107c9d552d6
bobthellorma <98739156+bobthellorma@users.noreply.github.com>
2022-10-24 12:48:02 +1030

    operation shown

--- main.cpp
+++ main.cpp
@@ -6,32 +6,72 @@
 #include <stack>
 #include <queue>
 
-int multiply(std::string a, std::string b)
+int multiply(std::string a, std::string b, int brac)
 {
     int aint = stoi(a);
     int bint = stoi(b);
-    return aint*bint;
+    int ans = aint * bint;
+    if (brac == 2)
+    {
+        std::cout << aint << " ";
+    }
+    std::cout << "* " << bint << " ";
+    if (brac == 0 || brac == 2)
+    {
+        std::cout << ") ";
+    }
+    return ans;
 }
 
-int divide (std::string a, std::string b)
+int divide (std::string a, std::string b, int brac)
 {
     int aint = stoi(a);
     int bint = stoi(b);
-    return aint/bint;
+    int ans = aint / bint;
+    if (brac == 2)
+    {
+        std::cout << aint << " ";
+    }
+    std::cout << "/ " << bint << " ";
+    if (brac == 0 || brac == 2)
+    {
+        std::cout << ") ";
+    }
+    return ans;
 }
 
-int add(std::string a, std::string b)
+int add(std::string a, std::string b, int brac)
 {
     int aint = stoi(a);
     int bint = stoi(b);
-    return aint + bint;
+    int ans = aint + bint;
+    if (brac == 2)
+    {
+        std::cout << aint << " ";
+    }
+    std::cout << "+ " << bint << " ";
+    if (brac == 0 || brac == 2)
+    {
+        std::cout << ") ";
+    }
+    return ans;
 }
 
-int subtract(std::string a, std::string b)
+int subtract(std::string a, std::string b, int brac)
 {
     int aint = stoi(a);
     int bint = stoi(b);
-    return aint - bint;
+    int ans = aint - bint;
+    if (brac == 2)
+    {
+        std::cout << aint << " ";
+    }
+    std::cout << "- " << bint << " ";
+    if (brac == 0 || brac == 2)
+    {
+        std::cout << ") ";
+    }
+    return ans;
 }
 
 void operate(std::vector<std::string> functionin)
@@ -47,8 +87,8 @@ void operate(std::vector<std::string> functionin)
         {
             if (swita == 1)
             {
-                //std::cout << "Error";
-                //return;
+                std::cout << "Error";
+                return;
             }
             operations.push(functionin.at(i));
         }
@@ -59,15 +99,41 @@ void operate(std::vector<std::string> functionin)
         }
     }
 
+    // Check for error
+    if (operations.size() != numbers.size()-1)
+    {
+        std::cout << "Error";
+        return;
+    }
+
+    // Opening brackets
+    int num = numbers.size();
+    for (int i = 0; i < num-1; i++)
+    {
+        std::cout << "( ";
+    }
+
     //Operate on the numbers
     int result;
+    int nobrackets = 0;
     while (numbers.size() != 1)
     {
+        nobrackets = 0;
+        if (numbers.size() == 2)
+        {
+            nobrackets = 1;
+        }
+
+        if (numbers.size() == num)
+        {
+            nobrackets = 2;
+        }
+
         // Obtain values for calculations - operation
         if(operations.empty())
         {
-            //std::cout << "Error";
-            //return;
+            std::cout << "Error";
+            return;
         }
         std::string operation = operations.top();
         operations.pop();
@@ -80,33 +146,33 @@ void operate(std::vector<std::string> functionin)
         // Make calculation
         if (operation == "*")
         {
-            result = multiply(firstval,secondval);
+            result = multiply(firstval,secondval, nobrackets);
         }
         else if (operation == "/")
         {
-            result = divide(firstval,secondval);
+            result = divide(firstval,secondval, nobrackets);
         }
         else if (operation == "+")
         {
-            result = add(firstval,secondval);
+            result = add(firstval,secondval, nobrackets);
         }
         else if (operation == "-")
         {
-            result = subtract(firstval,secondval);
+            result = subtract(firstval,secondval, nobrackets);
         }
 
         // update front of numbers queue
         std::string &first = numbers.front();
         first = std::to_string(result);
     }
-    //if (!operations.empty())
-    //{
-        //std::cout << "Error";
-    //}
-    //else
-    //{
-        std::cout << result;
-    //}
+    if (!operations.empty())
+    {
+        std::cout << "Error";
+    }
+    else
+    {
+        std::cout << "= " << result;
+    }
     return;
 }
 
