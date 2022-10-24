222e147d205f14b1520d2468bcd71c4d924b16c7
bobthellorma <98739156+bobthellorma@users.noreply.github.com>
2022-10-24 11:42:13 +1030

    passes initial tests

--- main.cpp
+++ main.cpp
@@ -1,117 +1,132 @@
 #include <iostream>
-#include "Node.h"
-#include "LinkedList.h"
-#include <string>
+#include <array>
 #include <vector>
+#include <string>
 #include <sstream>
+#include <stack>
+#include <queue>
+
+int multiply(std::string a, std::string b)
+{
+    int aint = stoi(a);
+    int bint = stoi(b);
+    return aint*bint;
+}
+
+int divide (std::string a, std::string b)
+{
+    int aint = stoi(a);
+    int bint = stoi(b);
+    return aint/bint;
+}
+
+int add(std::string a, std::string b)
+{
+    int aint = stoi(a);
+    int bint = stoi(b);
+    return aint + bint;
+}
+
+int subtract(std::string a, std::string b)
+{
+    int aint = stoi(a);
+    int bint = stoi(b);
+    return aint - bint;
+}
+
+void operate(std::vector<std::string> functionin)
+{
+    int swita;
+    std::stack<std::string> operations;
+    std::queue<std::string> numbers;
+
+    // Separate into operations and numbers
+    for (int i = 0; i < functionin.size(); i++)
+    {
+        if(functionin.at(i) == "+" || functionin.at(i) == "/" || functionin.at(i) == "*" || functionin.at(i) == "-")
+        {
+            if (swita == 1)
+            {
+                std::cout << "Error";
+                return;
+            }
+            operations.push(functionin.at(i));
+        }
+        else
+        {
+            swita = 1;
+            numbers.push(functionin.at(i));
+        }
+    }
+
+    //Operate on the numbers
+    int result;
+    while (numbers.size() != 1)
+    {
+        // Obtain values for calculations - operation
+        if(operations.empty())
+        {
+            std::cout << "Error";
+            return;
+        }
+        std::string operation = operations.top();
+        operations.pop();
+
+        // Obtain values for calculations - numbers
+        std::string firstval = numbers.front();
+        numbers.pop();
+        std::string secondval = numbers.front();
+
+        // Make calculation
+        if (operation == "*")
+        {
+            result = multiply(firstval,secondval);
+        }
+        else if (operation == "/")
+        {
+            result = divide(firstval,secondval);
+        }
+        else if (operation == "+")
+        {
+            result = add(firstval,secondval);
+        }
+        else if (operation == "-")
+        {
+            result = subtract(firstval,secondval);
+        }
+
+        // update front of numbers queue
+        std::string &first = numbers.front();
+        first = std::to_string(result);
+    }
+    if (!operations.empty())
+    {
+        std::cout << "Error";
+    }
+    else
+    {
+        std::cout << result;
+    }
+    return;
+}
 
 int main()
 {
-    // Take in line of input
-    std::string input;
-    std::getline(std::cin, input);
-    //std::cout << input.at(0);
 
-    // Find spaces
-    int spaces[100];
-    int numspaces;
-    for (int i = 0; i < input.size(); i++)
-    {
-        if (input.at(i) == 32)
-        {
-            spaces[numspaces] = input[i];
-            numspaces++;
-        }
-    }
+std::array<int,100> values;
+int size = 0;
 
-    
+// Get Input
+std::string input;
+std::getline(std::cin, input);
+std::istringstream ss(input);
 
-    
-    char first = input[input.size()-3];
-    char second = input[input.size()-1];
-    std::string stringedparam = {first,second};
-    int intparam = std::stoi(stringedparam);
-    int param1 = intparam/10;
-    int param2 = intparam%10;
-
-    // array condition
-    int arr[4] = {1,2,3,4};
-    if (input.at(0) == 53){
-        arr[0] = 5;
-        arr[1] = 2;
-        arr[2] = 7;
-        arr[3] = 10;
-        }
-    else if (input.at(0) == 51){
-        arr[0] = 3;
-        arr[1] = 4;
-        arr[2] = 2;
-        arr[3] = 1;
-    }
-    else{
-        arr[0] = 45;
-        arr[1] = 20;
-        arr[2] = 2;
-        arr[3] = 10;
-    }
-    int size = sizeof(arr)/sizeof(arr[0]);
-
-
-    // get std::string command
-    char sixthfromend = input.at(input.size()-6);
-    char fifth = input.at(input.size()-5);
-    std::string command;
-    int upto;
-    {
-        if (sixthfromend == 32) // if the character is only one
-        {
-            command = {fifth};
-        }
-        else // if two characters
-        {
-            command = {sixthfromend, fifth};
-        }
-    }
-    // std::cout << command << std::endl; // command is correct
-
-
-    // Create Linked List with inputs
-    LinkedList use = LinkedList(arr,size);
-
-    // Function Map
-    if (command == "AF")
-    {
-        use.addFront(param1);
-    }
-    else if (command == "AE")
-    {
-        use.addEnd(param1);
-    }
-    else if (command == "AP")
-    {
-        use.addAtPosition(param1,param2);
-    }
-    else if (command == "S")
-    {
-        use.search(param1);
-    }
-    else if (command == "DF")
-    {
-        use.deleteFront();
-    }
-    else if (command == "DE")
-    {
-        use.deleteEnd();
-    }
-    else if (command == "DP")
-    {
-        use.deletePosition(param1);
-    }
-    else if (command == "GI")
-    {
-        use.getItem(param1);
-    }
-
-    use.printItems();
-
+// Enter Input Into Vector
+std::vector<std::string> tokens;
+std::string token;
+while (ss >> token)
+{
+    tokens.push_back(token);
+}
+    operate(tokens);
 }
